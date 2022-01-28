8a8623d6052cada9dbfa4f97c2a96db8821ea291
Konstantin Nepovinnov <nekonstantin@yandex.ru>
2022-01-29 01:32:20 +0400

    start and finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    brackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (stack.length > 0) {
+      let openBracket = stack.pop();
+      if (brackets[openBracket] == str[i]) {
+        continue
+      }
+      stack.push(openBracket);
+    }
+    if (brackets[str[i]]) {
+      stack.push(str[i]);
+    } else {
+      return false;
+    }
+  }
+  return stack.length == 0;
 }
